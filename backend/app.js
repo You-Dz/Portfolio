19e1c0b395d3e6ce8ad5ee289b205811c2b17191
Yoann Dz <recodvz@gmail.com>
2026-07-08 10:19:42 +0200

    Fix: CORS and deploy

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,15 @@ connectDB();
 
 // CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-eight-beige-xjvnl7rg1t.vercel.app');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
+    // ← AJOUTE CES 2 LIGNES
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
     next();
 });
 app.use(express.json());
